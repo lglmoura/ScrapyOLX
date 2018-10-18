66cd83ad3741380475e2d88532f131e1915b4100
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-10-17 22:56:39 -0300

    Pegando a Lista de Carros

--- olx_scrapy/spiders/carros.py
+++ olx_scrapy/spiders/carros.py
@@ -4,8 +4,13 @@ import scrapy
 
 class CarrosSpider(scrapy.Spider):
     name = 'carros'
-    allowed_domains = ['https://rj.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios']
-    start_urls = ['http://https://rj.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/']
+    #allowed_domains = ['https://rj.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios']
+    start_urls = ['https://rj.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/']
 
     def parse(self, response):
-        pass
+        items = response.xpath(
+            '//ul[@id="main-ad-list"]/li'
+        )
+        for item in items:
+            self.log(item.xpath('./a/@href').extract_first())
+            
